aa72029d3d9f6a8673fd8da25fdb82395d94042c
RaadAlotaibi <38744438+RaadAlotaibi@users.noreply.github.com>
2018-05-04 20:15:10 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,13 @@
 <?php
 
-$servername = "localhost";
-$username = "ralotaibi";
-$password = "";
-$database = "mybooks_db";
-$conn = new mysqli($servername, $username, $password, $database);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
 
 
 if ($conn->connect_error) {
